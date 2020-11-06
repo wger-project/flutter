41c86777ea9cff4ba6245b7bf0fcdf5f62f8671b
Roland Geider <roland@geider.net>
2020-11-06 11:54:14 +0100

    Add exercise providers

---
+++ lib/providers/exercises.dart
@@ -0,0 +1,63 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:wger/providers/auth.dart';
+
+import 'exercise.dart';
+
+class Exercises with ChangeNotifier {
+  static const exercisesUrl = '/api/v2/exercise/?language=1&limit=1000';
+
+  String _url;
+  List<Exercise> _entries = [];
+  Auth _auth;
+
+  Exercises(Auth auth, List<Exercise> entries) {
+    this._auth = auth;
+    this._entries = entries;
+    this._url = auth.serverUrl + exercisesUrl;
+  }
+
+  List<Exercise> get items {
+    return [..._entries];
+  }
+
+  Exercise findById(int id) {
+    return _entries.firstWhere((workoutPlan) => workoutPlan.id == id);
+  }
+
+  Future<void> fetchAndSetExercises() async {
+    final response = await http.get(
+      _url,
+      headers: <String, String>{'Authorization': 'Token ${_auth.token}'},
+    );
+    final extractedData = json.decode(response.body) as Map<String, dynamic>;
+
+    final List<Exercise> loadedExercises = [];
+    if (loadedExercises == null) {
+      return;
+    }
+
+    try {
+      for (final entry in extractedData['results']) {
+        loadedExercises.add(Exercise(
+          id: entry['id'],
+          uuid: entry['uudid'],
+          name: entry['name'],
+          description: entry['description'],
+          category: entry['category'],
+          creation_date: DateTime.parse(entry['creation_date']),
+          muscles: entry['muscles'],
+          muscles_secondary: entry['muscles_secondary'],
+          equipment: entry['equipment'],
+        ));
+      }
+
+      _entries = loadedExercises;
+      notifyListeners();
+    } catch (error) {
+      throw (error);
+    }
+  }
+}
