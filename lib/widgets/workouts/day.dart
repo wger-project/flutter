fc74e3444875364534792245f377977f9b6919a4
Roland Geider <roland@geider.net>
2020-11-08 19:34:12 +0100

    Don't nest ListViews

--- lib/widgets/workouts/day.dart
+++ lib/widgets/workouts/day.dart
@@ -12,42 +12,23 @@ class WorkoutDayWidget extends StatelessWidget {
       children: [
         Padding(
           padding: const EdgeInsets.symmetric(vertical: 10),
-          child: Text(
-            'Day #${_day.id} - ${_day.description}',
-            style: Theme.of(context).textTheme.headline6,
-          ),
+        ),
+        Text(
+          'Day #${_day.id} - ${_day.description}',
+          style: Theme.of(context).textTheme.headline6,
         ),
         Divider(),
-        Container(
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: _day.sets.length,
-            itemBuilder: (context, index) {
-              return Card(
-                child: Column(
-                  children: [
-                    ListTile(
-                      leading: Text('#${_day.sets[index].order}'),
-                      title: Text('Exercise name here'),
-                      subtitle: Text('Set info here'),
-                    ),
-                  ],
+        Column(
+          children: _day.sets
+              .map(
+                (e) => ListTile(
+                  leading: Text('#${e.order}'),
+                  title: Text('Exercise name here'),
+                  subtitle: Text('Set info here'),
                 ),
-              );
-            },
-          ),
+              )
+              .toList(),
         ),
-        /*
-        Container(
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: _day.sets.length,
-            itemBuilder: (context, index) {
-              return Text('Set #${_day.sets[index].id}');
-            },
-          ),
-        ),
-         */
       ],
     );
   }
