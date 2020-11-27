4d956211dde946f3f4acca360d450f41c5e754c3
Roland Geider <roland@geider.net>
2020-11-27 20:11:59 +0100

    Update theme

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
 
 const Color wgerPrimaryColor = Color(0xff2a4c7d);
+const Color wgerPrimaryButtonColor = Color(0xff266dd3);
 const Color wgerSecondaryColor = Color(0xffe63946);
 
 final ThemeData wgerTheme = ThemeData(
-  primaryColor: wgerPrimaryColor,
-  accentColor: wgerSecondaryColor,
+    /*
+   * General stuff
+   */
+    primaryColor: wgerPrimaryColor,
+    accentColor: wgerSecondaryColor,
 
-  // This makes the visual density adapt to the platform that you run
-  // the app on. For desktop platforms, the controls will be smaller and
-  // closer together (more dense) than on mobile platforms.
-  visualDensity: VisualDensity.adaptivePlatformDensity,
+    // This makes the visual density adapt to the platform that you run
+    // the app on. For desktop platforms, the controls will be smaller and
+    // closer together (more dense) than on mobile platforms.
+    visualDensity: VisualDensity.adaptivePlatformDensity,
 
-  textTheme: TextTheme(
-    //headline1: TextStyle(fontSize: 30.0, fontFamily: "OpenSansBold"),
-    headline2: TextStyle(fontSize: 30.0, fontFamily: "OpenSansBold"),
-    //headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-  ),
-);
+    /*
+     * Text theme
+     */
+    textTheme: TextTheme(
+      //headline1: TextStyle(fontSize: 30.0, fontFamily: "OpenSansBold"),
+      headline2: TextStyle(fontSize: 30.0, fontFamily: "OpenSansBold"),
+      //headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+    ),
+
+    /*
+     * Button theme
+     */
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        primary: wgerPrimaryButtonColor,
+      ),
+    ),
+    outlinedButtonTheme: OutlinedButtonThemeData(
+      style: OutlinedButton.styleFrom(
+        primary: wgerPrimaryButtonColor,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        primary: wgerPrimaryButtonColor,
+      ),
+    ),
+
+    /*
+    * Forms, etc.
+    */
+    sliderTheme: SliderThemeData(
+      activeTrackColor: wgerPrimaryButtonColor,
+      thumbColor: wgerPrimaryColor,
+    ));
