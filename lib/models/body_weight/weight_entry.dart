3bbd22c3d508c250bd68fc0f05969b683c01cc96
Roland Geider <roland@geider.net>
2020-12-06 22:48:41 +0100

    Remove unused import

--- lib/models/body_weight/weight_entry.dart
+++ lib/models/body_weight/weight_entry.dart
@@ -16,7 +16,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import 'package:flutter/foundation.dart';
 import 'package:json_annotation/json_annotation.dart';
 import 'package:wger/helpers/json.dart';
 
