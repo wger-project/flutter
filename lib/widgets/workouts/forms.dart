ec2325b3e292ea7223ccdd7bbbc68e45227484e5
Roland Geider <roland@geider.net>
2021-04-08 13:09:28 +0200

    Redirect to the correct screen after editing a workout plan

--- lib/widgets/workouts/forms.dart
+++ lib/widgets/workouts/forms.dart
@@ -82,10 +82,10 @@ class WorkoutForm extends StatelessWidget {
                   ? await Provider.of<WorkoutPlans>(context, listen: false).editWorkout(_plan)
                   : await Provider.of<WorkoutPlans>(context, listen: false).addWorkout(_plan);
 
-              Navigator.of(context).pop();
-              if (_plan.id == null) {
-                Navigator.of(context).pushNamed(WorkoutPlanScreen.routeName, arguments: _plan);
-              }
+              Navigator.of(context).pushReplacementNamed(
+                WorkoutPlanScreen.routeName,
+                arguments: _plan,
+              );
             },
           ),
         ],
