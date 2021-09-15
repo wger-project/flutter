f90ddc9366fdebb6ddf4105897b6ab96644d871d
Roland Geider <roland@geider.net>
2021-09-15 20:41:59 +0200

    Improve exercise initial loading and reading from cache

--- lib/models/exercises/comment.g.dart
+++ lib/models/exercises/comment.g.dart
@@ -7,14 +7,16 @@ part of 'comment.dart';
 // **************************************************************************
 
 Comment _$CommentFromJson(Map<String, dynamic> json) {
-  $checkKeys(json, requiredKeys: const ['id', 'comment']);
+  $checkKeys(json, requiredKeys: const ['id', 'exercise', 'comment']);
   return Comment(
     id: json['id'] as int,
+    exerciseId: json['exercise'] as int,
     comment: json['comment'] as String,
   );
 }
 
 Map<String, dynamic> _$CommentToJson(Comment instance) => <String, dynamic>{
       'id': instance.id,
+      'exercise': instance.exerciseId,
       'comment': instance.comment,
     };
