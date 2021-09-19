adb5b6317e97cab4152c65cd677234d03deec039
Roland Geider <roland@geider.net>
2021-09-19 20:30:54 +0200

    If no wger API key is present launch a browser so they can register manually

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -22,6 +22,7 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       body: Center(
         child: Text('Loading...'),
       ),
