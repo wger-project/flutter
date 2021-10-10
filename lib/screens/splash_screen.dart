c17076005c3936d93ec3feaa6e9046769489a6d3
Roland Geider <roland@geider.net>
2021-10-10 19:24:57 +0200

    Remove background color, change was committed by mistake

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -22,7 +22,6 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
       body: Center(
         child: Text('Loading...'),
       ),
