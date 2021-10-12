ee3df1badaebc3448283b2c52f56fd8317d2486d
Sandi Milohanić <sandi.milohanic@gmail.com>
2021-10-12 18:43:45 +0200

    #29 - Add minimum required server version

---
+++ lib/screens/update_app_screen.dart
@@ -0,0 +1,36 @@
+/*
+ * This file is part of wger Workout Manager <https://github.com/wger-project>.
+ * Copyright (C) 2020, 2021 wger Team
+ *
+ * wger Workout Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class UpdateAppScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AlertDialog(
+        title: Text(
+          AppLocalizations.of(context).appUpdateTitle,
+          style: TextStyle(fontSize: 24),
+        ),
+        content: Text(AppLocalizations.of(context).appUpdateContent),
+        actions: null,
+      ),
+    );
+  }
+}
