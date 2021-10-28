ea8eada7ac30d4c474f289aae590a9a9086fd18a
Roland Geider <roland@geider.net>
2021-10-28 11:43:44 +0200

    Commit generated mock files

--- test/measurements/measurement_categories_screen_test.mocks.dart
+++ test/measurements/measurement_categories_screen_test.mocks.dart
@@ -19,36 +19,41 @@ import 'package:wger/providers/measurement.dart' as _i4;
 // ignore_for_file: prefer_const_constructors
 // ignore_for_file: unnecessary_parenthesis
 
-class _FakeWgerBaseProvider_0 extends _i1.Fake implements _i2.WgerBaseProvider {}
+class _FakeWgerBaseProvider_0 extends _i1.Fake implements _i2.WgerBaseProvider {
+}
 
-class _FakeMeasurementCategory_1 extends _i1.Fake implements _i3.MeasurementCategory {}
+class _FakeMeasurementCategory_1 extends _i1.Fake
+    implements _i3.MeasurementCategory {}
 
 /// A class which mocks [MeasurementProvider].
 ///
 /// See the documentation for Mockito's code generation for more information.
-class MockMeasurementProvider extends _i1.Mock implements _i4.MeasurementProvider {
+class MockMeasurementProvider extends _i1.Mock
+    implements _i4.MeasurementProvider {
   MockMeasurementProvider() {
     _i1.throwOnMissingStub(this);
   }
 
   @override
   _i2.WgerBaseProvider get baseProvider =>
-      (super.noSuchMethod(Invocation.getter(#baseProvider), returnValue: _FakeWgerBaseProvider_0())
-          as _i2.WgerBaseProvider);
+      (super.noSuchMethod(Invocation.getter(#baseProvider),
+          returnValue: _FakeWgerBaseProvider_0()) as _i2.WgerBaseProvider);
   @override
   List<_i3.MeasurementCategory> get categories =>
-      (super.noSuchMethod(Invocation.getter(#categories), returnValue: <_i3.MeasurementCategory>[])
+      (super.noSuchMethod(Invocation.getter(#categories),
+              returnValue: <_i3.MeasurementCategory>[])
           as List<_i3.MeasurementCategory>);
   @override
   bool get hasListeners =>
-      (super.noSuchMethod(Invocation.getter(#hasListeners), returnValue: false) as bool);
+      (super.noSuchMethod(Invocation.getter(#hasListeners), returnValue: false)
+          as bool);
   @override
-  void clear() =>
-      super.noSuchMethod(Invocation.method(#clear, []), returnValueForMissingStub: null);
+  void clear() => super.noSuchMethod(Invocation.method(#clear, []),
+      returnValueForMissingStub: null);
   @override
-  _i3.MeasurementCategory findCategoryById(int? id) =>
-      (super.noSuchMethod(Invocation.method(#findCategoryById, [id]),
-          returnValue: _FakeMeasurementCategory_1()) as _i3.MeasurementCategory);
+  _i3.MeasurementCategory findCategoryById(int? id) => (super.noSuchMethod(
+      Invocation.method(#findCategoryById, [id]),
+      returnValue: _FakeMeasurementCategory_1()) as _i3.MeasurementCategory);
   @override
   _i5.Future<void> fetchAndSetCategories() =>
       (super.noSuchMethod(Invocation.method(#fetchAndSetCategories, []),
@@ -60,10 +65,10 @@ class MockMeasurementProvider extends _i1.Mock implements _i4.MeasurementProvide
           returnValue: Future<void>.value(),
           returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
-  _i5.Future<void> fetchAndSetAllCategoriesAndEntries() =>
-      (super.noSuchMethod(Invocation.method(#fetchAndSetAllCategoriesAndEntries, []),
-          returnValue: Future<void>.value(),
-          returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
+  _i5.Future<void> fetchAndSetAllCategoriesAndEntries() => (super.noSuchMethod(
+      Invocation.method(#fetchAndSetAllCategoriesAndEntries, []),
+      returnValue: Future<void>.value(),
+      returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
   _i5.Future<void> addCategory(_i3.MeasurementCategory? category) =>
       (super.noSuchMethod(Invocation.method(#addCategory, [category]),
@@ -76,7 +81,8 @@ class MockMeasurementProvider extends _i1.Mock implements _i4.MeasurementProvide
           returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
   _i5.Future<void> editCategory(int? id, String? newName, String? newUnit) =>
-      (super.noSuchMethod(Invocation.method(#editCategory, [id, newName, newUnit]),
+      (super.noSuchMethod(
+          Invocation.method(#editCategory, [id, newName, newUnit]),
           returnValue: Future<void>.value(),
           returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
@@ -90,25 +96,28 @@ class MockMeasurementProvider extends _i1.Mock implements _i4.MeasurementProvide
           returnValue: Future<void>.value(),
           returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
-  _i5.Future<void> editEntry(
-          int? id, int? categoryId, num? newValue, String? newNotes, DateTime? newDate) =>
+  _i5.Future<void> editEntry(int? id, int? categoryId, num? newValue,
+          String? newNotes, DateTime? newDate) =>
       (super.noSuchMethod(
-          Invocation.method(#editEntry, [id, categoryId, newValue, newNotes, newDate]),
+          Invocation.method(
+              #editEntry, [id, categoryId, newValue, newNotes, newDate]),
           returnValue: Future<void>.value(),
           returnValueForMissingStub: Future<void>.value()) as _i5.Future<void>);
   @override
   String toString() => super.toString();
   @override
-  void addListener(_i7.VoidCallback? listener) => super
-      .noSuchMethod(Invocation.method(#addListener, [listener]), returnValueForMissingStub: null);
+  void addListener(_i7.VoidCallback? listener) =>
+      super.noSuchMethod(Invocation.method(#addListener, [listener]),
+          returnValueForMissingStub: null);
   @override
   void removeListener(_i7.VoidCallback? listener) =>
       super.noSuchMethod(Invocation.method(#removeListener, [listener]),
           returnValueForMissingStub: null);
   @override
-  void dispose() =>
-      super.noSuchMethod(Invocation.method(#dispose, []), returnValueForMissingStub: null);
+  void dispose() => super.noSuchMethod(Invocation.method(#dispose, []),
+      returnValueForMissingStub: null);
   @override
   void notifyListeners() =>
-      super.noSuchMethod(Invocation.method(#notifyListeners, []), returnValueForMissingStub: null);
+      super.noSuchMethod(Invocation.method(#notifyListeners, []),
+          returnValueForMissingStub: null);
 }
