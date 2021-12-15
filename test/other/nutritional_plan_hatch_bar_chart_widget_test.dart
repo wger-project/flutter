328520780f3f6db77c7ffe1b7c7fcf7502de1987
Ady <adyhnat@gmail.com>
2021-12-15 09:43:22 +0100

    74% tests for nutritional plan hatch bar chart widget.

--- test/other/nutritional_plan_hatch_bar_chart_widget_test.dart
+++ test/other/nutritional_plan_hatch_bar_chart_widget_test.dart
@@ -2,44 +2,59 @@ import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:wger/models/nutrition/log.dart';
+import 'package:wger/models/nutrition/meal.dart';
+import 'package:wger/models/nutrition/nutritional_plan.dart';
 import 'package:wger/widgets/nutrition/charts.dart';
 import 'package:wger/widgets/nutrition/helpers.dart';
 
 import '../../test_data/nutritional_plans.dart';
+import '../nutrition/nutritional_plan_form_test.mocks.dart';
+import 'nutritional_plan_service_mock.dart';
 
 void main() {
 
-  Widget getWidget({locale = 'en'}) {
+  final plan1 = NutritionalPlan.empty();
+  final plan2 = getNutritionalPlan();
+  final plan3 = getNutritionalPlan();
+  var meal1 = Meal();
+
+  Widget getWidget(NutritionalPlan nutritionalPlan, {locale = 'en'}) {
     return MaterialApp(
       locale: Locale(locale),
       localizationsDelegates: AppLocalizations.localizationsDelegates,
       supportedLocales: AppLocalizations.supportedLocales,
-      home: NutritionalPlanHatchBarChartWidget(getNutritionalPlan()),
+      home: NutritionalPlanHatchBarChartWidget(nutritionalPlan),
     );
   }
 
-  testWidgets('selection can be set programmatically',
+  setUp(() {
+    meal1 = plan3.meals.first;
+
+    final now = DateTime.now();
+
+    for(var i = 0; i <= 7; i++) {
+      var key = DateTime(now.year, now.month, now.day -(1*i));
+      plan3.logs.add(Log.fromMealItem(meal1.mealItems.first, 1, 1, key));
+    }
+  });
+
+
+  testWidgets('Test the widget on a nutritional plan with and without '
+      'nutritional values',
           (WidgetTester tester) async {
 
-        await tester.pumpWidget(getWidget());
-        await tester.tap(find.byType(charts.BarChart));
+        await tester.pumpWidget(getWidget(plan2));
+        expect(find.byType(charts.BarChart), findsOneWidget);
+        await tester.pumpWidget(getWidget(plan1));
+        expect(find.byType(Container), findsOneWidget);
+
+        await tester.pumpWidget(getWidget(plan3));
+        expect(find.byType(charts.BarChart), findsOneWidget);
+
+
         await tester.pumpAndSettle();
-
-        //expect(find.text('Category'), findsOneWidget);
-
-        /*final onTapSelection =
-        charts.UserManagedSelectionModel<String>.fromConfig(
-            selectedDataConfig: [
-              charts.SeriesDatumConfig<String>('Sales', '2016')
-            ]);
-
-        charts.SelectionModel<String>? currentSelectionModel;
-
-        void selectionChangedListener(charts.SelectionModel<String> model) {
-          currentSelectionModel = model;
-        }*/
-
-        //final testChart = TestChart(selectionChangedListener, onTapSelection);
 
       });
 }
