2ed6d4d11d62641c7f4c8f9ab6cdd2f8066de3e7
Roland Geider <roland@geider.net>
2022-01-29 11:20:09 +0100

    Backport new exercise detail page from exercise-crowdsourcing branch

--- lib/widgets/exercises/exercises.dart
+++ lib/widgets/exercises/exercises.dart
@@ -20,10 +20,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 import 'package:wger/models/exercises/exercise.dart';
+import 'package:wger/models/exercises/muscle.dart';
+import 'package:wger/providers/exercises.dart';
+import 'package:wger/widgets/core/core.dart';
+import 'package:wger/widgets/exercises/images.dart';
 
 class ExerciseDetail extends StatelessWidget {
   final Exercise _exercise;
+  static const PADDING = 9.0;
 
   const ExerciseDetail(this._exercise);
 
@@ -34,53 +41,150 @@ class ExerciseDetail extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // Category
-          Text(
-            AppLocalizations.of(context).category,
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          Text(_exercise.categoryObj.name),
-          const SizedBox(height: 8),
-
-          // Equipment
-          Text(
-            AppLocalizations.of(context).equipment,
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          if (_exercise.equipment.isNotEmpty)
-            Text(_exercise.equipment.map((e) => e.name).toList().join('\n')),
-          if (_exercise.equipment.isEmpty) const Text('-/-'),
-          const SizedBox(height: 8),
-
-          // Muscles
-          Text(
-            AppLocalizations.of(context).muscles,
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          if (_exercise.muscles.isNotEmpty)
-            Text(_exercise.muscles.map((e) => e.name).toList().join('\n')),
-          if (_exercise.muscles.isEmpty) const Text('-/-'),
-          const SizedBox(height: 8),
-
-          // Muscles secondary
-          Text(
-            AppLocalizations.of(context).musclesSecondary,
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          if (_exercise.musclesSecondary.isNotEmpty)
-            Text(_exercise.musclesSecondary.map((e) => e.name).toList().join('\n')),
-          if (_exercise.musclesSecondary.isEmpty) const Text('-/-'),
-          const SizedBox(height: 8),
+          // Images
+          ...getImages(),
 
           // Description
-          Text(
-            AppLocalizations.of(context).description,
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          Html(data: _exercise.description),
-          const SizedBox(height: 8),
+          ...getDescription(context),
+
+          // Notes
+          ...getNotes(context),
+
+          // Muscles
+          ...getMuscles(context),
         ],
       ),
+    );
+  }
+
+  List<Widget> getNotes(BuildContext context) {
+    final List<Widget> out = [];
+    if (_exercise.tips.isNotEmpty) {
+      out.add(Text(
+        AppLocalizations.of(context).notes,
+        style: Theme.of(context).textTheme.headline5,
+      ));
+      for (final e in _exercise.tips) {
+        out.add(Text(e.comment));
+      }
+      out.add(const SizedBox(height: PADDING));
+    }
+
+    return out;
+  }
+
+  List<Widget> getMuscles(BuildContext context) {
+    final List<Widget> out = [];
+    out.add(Text(
+      AppLocalizations.of(context).muscles,
+      style: Theme.of(context).textTheme.headline5,
+    ));
+    out.add(Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: PADDING),
+            child: MuscleWidget(
+              muscles: _exercise.muscles,
+              musclesSecondary: _exercise.musclesSecondary,
+              isFront: true,
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: PADDING),
+            child: MuscleWidget(
+              muscles: _exercise.muscles,
+              musclesSecondary: _exercise.musclesSecondary,
+              isFront: false,
+            ),
+          ),
+        ),
+      ],
+    ));
+
+    out.add(Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          children: [
+            Text(
+              AppLocalizations.of(context).muscles,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            ..._exercise.muscles.map((e) => Text(e.name)).toList(),
+          ],
+        ),
+        Column(
+          children: [
+            Text(
+              AppLocalizations.of(context).musclesSecondary,
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            ..._exercise.musclesSecondary.map((e) => Text(e.name)).toList(),
+          ],
+        ),
+      ],
+    ));
+    out.add(const SizedBox(height: PADDING));
+
+    return out;
+  }
+
+  List<Widget> getDescription(BuildContext context) {
+    final List<Widget> out = [];
+    out.add(Text(
+      AppLocalizations.of(context).description,
+      style: Theme.of(context).textTheme.headline5,
+    ));
+    out.add(Html(data: _exercise.description));
+
+    return out;
+  }
+
+  List<Widget> getImages() {
+    // TODO: add carousel for the other images
+    final List<Widget> out = [];
+    out.add(ExerciseImageWidget(image: _exercise.getMainImage));
+    out.add(const SizedBox(height: PADDING));
+
+    return out;
+  }
+}
+
+class MuscleWidget extends StatelessWidget {
+  late final List<Muscle> muscles;
+  late final List<Muscle> musclesSecondary;
+  final bool isFront;
+
+  MuscleWidget({
+    List<Muscle> muscles = const [],
+    List<Muscle> musclesSecondary = const [],
+    this.isFront = true,
+  }) {
+    this.muscles = muscles.where((m) => m.isFront == isFront).toList();
+    this.musclesSecondary = musclesSecondary.where((m) => m.isFront == isFront).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final background = isFront ? 'front' : 'back';
+    final muscleFolder = isFront ? 'main' : 'secondary';
+
+    return Stack(
+      children: [
+        SvgPicture.asset('assets/images/muscles/$background.svg'),
+        ...muscles
+            .map((m) => SvgPicture.asset('assets/images/muscles/main/muscle-${m.id}.svg'))
+            .toList(),
+        ...musclesSecondary
+            .map((m) => SvgPicture.asset('assets/images/muscles/$muscleFolder/muscle-${m.id}.svg'))
+            .toList(),
+      ],
     );
   }
 }
