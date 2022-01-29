dd94f31e926f84e7b61aab217c36f46babd17e56
Roland Geider <roland@geider.net>
2022-01-29 14:38:57 +0100

    Add a controls overlay on the exercise video widget
    This is much cleaner and also more intuitive

--- lib/widgets/exercises/videos.dart
+++ lib/widgets/exercises/videos.dart
@@ -54,21 +54,87 @@ class _ExerciseVideoWidgetState extends State<ExerciseVideoWidget> {
   @override
   Widget build(BuildContext context) {
     return _controller.value.isInitialized
-        ? Column(
-            children: [
-              AspectRatio(
-                aspectRatio: _controller.value.aspectRatio,
-                child: VideoPlayer(_controller),
-              ),
-              IconButton(
-                  onPressed: () {
-                    setState(() {
-                      _controller.value.isPlaying ? _controller.pause() : _controller.play();
-                    });
-                  },
-                  icon: Icon(_controller.value.isPlaying ? Icons.stop : Icons.play_arrow))
-            ],
-          )
+        ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: Stack(alignment: Alignment.bottomCenter, children: [
+              VideoPlayer(_controller),
+              _ControlsOverlay(controller: _controller),
+              VideoProgressIndicator(_controller, allowScrubbing: true),
+            ]))
         : Container();
+  }
+}
+
+/// Control overlay for the exercise video player
+///
+/// Taken from this example: https://pub.dev/packages/video_player/example
+class _ControlsOverlay extends StatelessWidget {
+  const _ControlsOverlay({Key? key, required this.controller}) : super(key: key);
+
+  static const _playbackRates = [
+    0.25,
+    0.5,
+    1.0,
+  ];
+
+  final VideoPlayerController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        AnimatedSwitcher(
+          duration: const Duration(milliseconds: 50),
+          reverseDuration: const Duration(milliseconds: 200),
+          child: controller.value.isPlaying
+              ? const SizedBox.shrink()
+              : Container(
+                  color: Colors.black26,
+                  child: const Center(
+                    child: Icon(
+                      Icons.play_arrow,
+                      color: Colors.white,
+                      size: 100.0,
+                      semanticLabel: 'Play',
+                    ),
+                  ),
+                ),
+        ),
+        GestureDetector(
+          onTap: () {
+            controller.value.isPlaying ? controller.pause() : controller.play();
+          },
+        ),
+        Align(
+          alignment: Alignment.topRight,
+          child: PopupMenuButton<double>(
+            initialValue: controller.value.playbackSpeed,
+            tooltip: 'Playback speed',
+            onSelected: (speed) {
+              controller.setPlaybackSpeed(speed);
+            },
+            itemBuilder: (context) {
+              return [
+                for (final speed in _playbackRates)
+                  PopupMenuItem(
+                    value: speed,
+                    child: Text('${speed}x'),
+                  )
+              ];
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                // Using less vertical padding as the text is also longer
+                // horizontally, so it feels like it would need more spacing
+                // horizontally (matching the aspect ratio of the video).
+                vertical: 8,
+                horizontal: 9,
+              ),
+              child: Text('${controller.value.playbackSpeed}x'),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
