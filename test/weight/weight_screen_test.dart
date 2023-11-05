2ff02c0935a257e3a0b87efd72ef8ce3574867aa
Roland Geider <roland@geider.net>
2023-11-05 15:14:19 +0100

    Fix test

--- test/weight/weight_screen_test.dart
+++ test/weight/weight_screen_test.dart
@@ -84,8 +84,8 @@ void main() {
     await tester.pumpWidget(createWeightScreen());
 
     // One in the entries list, one in the chart
-    expect(find.text('1/1/2021'), findsNWidgets(2));
-    expect(find.text('1/10/2021'), findsNWidgets(2));
+    expect(find.text('1/1/2021'), findsOneWidget);
+    expect(find.text('1/10/2021'), findsOneWidget);
   });
 
   testWidgets('Tests the localization of dates - DE', (WidgetTester tester) async {
