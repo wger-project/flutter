dfa73f323ef2035b90232eb333f5ca582a2f31f7
Miroslav Mazel <contact.mirek+git@protonmail.com>
2023-11-11 18:10:47 +0100

    Organize imports

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -16,11 +16,10 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'dart:ui';
 
+import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:table_calendar/table_calendar.dart';
 
 // Color scheme, please consult
