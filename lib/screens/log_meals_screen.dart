4249abf266af17e718f0f619b0e713476b5c32ba
Dieter Plaetinck <dieter@plaetinck.be>
2024-06-05 18:00:14 +0300

    cleanup

--- lib/screens/log_meals_screen.dart
+++ lib/screens/log_meals_screen.dart
@@ -17,14 +17,8 @@
  */
 
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:provider/provider.dart';
-import 'package:wger/models/nutrition/meal.dart';
 import 'package:wger/models/nutrition/nutritional_plan.dart';
-import 'package:wger/providers/nutrition.dart';
-import 'package:wger/screens/log_meal_screen.dart';
 import 'package:wger/widgets/nutrition/meal.dart';
-import 'package:wger/widgets/nutrition/widgets.dart';
 
 class LogMealsScreen extends StatefulWidget {
   static const routeName = '/log-meals';
