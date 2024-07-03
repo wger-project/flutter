395d40ce9ae6c6f27840f730274bb45e383a93c0
Dieter Plaetinck <dieter@plaetinck.be>
2024-07-03 20:58:45 +0300

    dart fix

--- lib/widgets/nutrition/helpers.dart
+++ lib/widgets/nutrition/helpers.dart
@@ -16,7 +16,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:provider/provider.dart';
