555f57d2deb269b000b8dcf7ae5908a13d9edbb9
Dieter Plaetinck <dieter@plaetinck.be>
2024-10-15 22:56:32 +0300

    dart fix --apply

--- lib/providers/nutrition.dart
+++ lib/providers/nutrition.dart
@@ -341,7 +341,7 @@ class NutritionPlansProvider with ChangeNotifier {
   /// Loads the available ingredients from the local cache
   Future<void> fetchIngredientsFromCache() async {
     final ingredientDb = await database.select(database.ingredients).get();
-    log("Read ${ingredientDb.length} ingredients from db cache");
+    log('Read ${ingredientDb.length} ingredients from db cache');
     if (ingredientDb.isNotEmpty) {
       ingredients = ingredientDb.map((e) => Ingredient.fromJson(jsonDecode(e.data))).toList();
     }
