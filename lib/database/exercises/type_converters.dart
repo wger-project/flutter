6ce57af1316d20415ebac7839b9371026eebde37
Roland Geider <roland@geider.net>
2025-03-31 12:20:54 +0200

    Exclude some files from the coverage report

--- lib/database/exercises/type_converters.dart
+++ lib/database/exercises/type_converters.dart
@@ -1,67 +1,11 @@
 import 'dart:convert';
 
 import 'package:drift/drift.dart';
-import 'package:wger/models/exercises/alias.dart';
 import 'package:wger/models/exercises/category.dart';
-import 'package:wger/models/exercises/comment.dart';
 import 'package:wger/models/exercises/equipment.dart';
-import 'package:wger/models/exercises/exercise.dart';
-import 'package:wger/models/exercises/image.dart';
 import 'package:wger/models/exercises/language.dart';
 import 'package:wger/models/exercises/muscle.dart';
-import 'package:wger/models/exercises/translation.dart';
 import 'package:wger/models/exercises/variation.dart';
-import 'package:wger/models/exercises/video.dart';
-
-class ExerciseBaseConverter extends TypeConverter<Exercise, String> {
-  const ExerciseBaseConverter();
-
-  @override
-  Exercise fromSql(String fromDb) {
-    final Map<String, dynamic> baseData = json.decode(fromDb);
-
-    final category = ExerciseCategory.fromJson(baseData['categories']);
-    final musclesPrimary = baseData['muscless'].map((e) => Muscle.fromJson(e)).toList();
-    final musclesSecondary = baseData['musclesSecondary'].map((e) => Muscle.fromJson(e)).toList();
-    final equipment = baseData['equipments'].map((e) => Equipment.fromJson(e)).toList();
-    final images = baseData['images'].map((e) => ExerciseImage.fromJson(e)).toList();
-    final videos = baseData['videos'].map((e) => Video.fromJson(e)).toList();
-
-    final List<Translation> translations = [];
-    for (final exerciseData in baseData['translations']) {
-      final translation = Translation(
-        id: exerciseData['id'],
-        name: exerciseData['name'],
-        description: exerciseData['description'],
-        exerciseId: baseData['id'],
-      );
-      translation.aliases = exerciseData['aliases'].map((e) => Alias.fromJson(e)).toList();
-      translation.notes = exerciseData['notes'].map((e) => Comment.fromJson(e)).toList();
-      translation.language = Language.fromJson(exerciseData['languageObj']);
-      translations.add(translation);
-    }
-
-    final exerciseBase = Exercise(
-      id: baseData['id'],
-      uuid: baseData['uuid'],
-      created: null,
-      //creationDate: toDate(baseData['creation_date']),
-      musclesSecondary: musclesSecondary.cast<Muscle>(),
-      muscles: musclesPrimary.cast<Muscle>(),
-      equipment: equipment.cast<Equipment>(),
-      category: category,
-      images: images.cast<ExerciseImage>(),
-      translations: translations,
-      videos: videos.cast<Video>(),
-    );
-    return exerciseBase;
-  }
-
-  @override
-  String toSql(Exercise value) {
-    return json.encode(value.toJson());
-  }
-}
 
 class MuscleConverter extends TypeConverter<Muscle, String> {
   const MuscleConverter();
