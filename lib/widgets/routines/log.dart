d663bcd88a69f9ccece9237df936ecd03f1b5933
Roland Geider <roland@geider.net>
2025-05-09 21:12:49 +0200

    Move common logic to SessionForm
    Refactor and polish a bit the form in the log overview.

--- lib/widgets/routines/log.dart
+++ lib/widgets/routines/log.dart
@@ -18,73 +18,87 @@
 
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:provider/provider.dart';
 import 'package:wger/helpers/colors.dart';
-import 'package:wger/helpers/json.dart';
 import 'package:wger/helpers/misc.dart';
 import 'package:wger/helpers/ui.dart';
 import 'package:wger/l10n/generated/app_localizations.dart';
 import 'package:wger/models/workouts/log.dart';
 import 'package:wger/models/workouts/routine.dart';
 import 'package:wger/models/workouts/session.dart';
-import 'package:wger/providers/routines.dart';
 import 'package:wger/widgets/measurements/charts.dart';
 import 'package:wger/widgets/routines/charts.dart';
+import 'package:wger/widgets/routines/forms/session.dart';
 
-class SessionInfo extends StatelessWidget {
+class SessionInfo extends StatefulWidget {
   final WorkoutSession _session;
 
   const SessionInfo(this._session);
 
   @override
+  State<SessionInfo> createState() => _SessionInfoState();
+}
+
+class _SessionInfoState extends State<SessionInfo> {
+  bool editMode = false;
+
+  @override
   Widget build(BuildContext context) {
     final i18n = AppLocalizations.of(context);
 
-    return GestureDetector(
-      onTap: (){
-        final routinesProvider = context.read<RoutinesProvider>();
-        showEditSessionDialog(context, _session, routinesProvider);
-
-      },
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          ListTile(
+            title: Text(
               i18n.workoutSession,
               style: Theme.of(context).textTheme.headlineSmall,
             ),
-            Text(
-              DateFormat.yMd(Localizations.localeOf(context).languageCode).format(_session.date),
-              style: Theme.of(context).textTheme.headlineSmall,
+            subtitle: Text(
+              DateFormat.yMd(Localizations.localeOf(context).languageCode)
+                  .format(widget._session.date),
             ),
-            const SizedBox(height: 8.0),
-            _buildInfoRow(
-              context,
-              i18n.timeStart,
-              _session.timeStart != null
-                  ? MaterialLocalizations.of(context).formatTimeOfDay(_session.timeStart!)
-                  : '-/-',
+            onTap: () => setState(() => editMode = !editMode),
+            trailing: Icon(editMode ? Icons.edit_off : Icons.edit),
+            contentPadding: EdgeInsets.zero,
+          ),
+          if (editMode)
+            SessionForm(
+              widget._session.routineId,
+              onSaved: () => setState(() => editMode = false),
+              session: widget._session,
+            )
+          else
+            Column(
+              children: [
+                _buildInfoRow(
+                  context,
+                  i18n.timeStart,
+                  widget._session.timeStart != null
+                      ? MaterialLocalizations.of(context)
+                          .formatTimeOfDay(widget._session.timeStart!)
+                      : '-/-',
+                ),
+                _buildInfoRow(
+                  context,
+                  i18n.timeEnd,
+                  widget._session.timeEnd != null
+                      ? MaterialLocalizations.of(context).formatTimeOfDay(widget._session.timeEnd!)
+                      : '-/-',
+                ),
+                _buildInfoRow(
+                  context,
+                  i18n.impression,
+                  widget._session.impressionAsString,
+                ),
+                _buildInfoRow(
+                  context,
+                  i18n.notes,
+                  widget._session.notes.isNotEmpty ? widget._session.notes : '-/-',
+                ),
+              ],
             ),
-            _buildInfoRow(
-              context,
-              i18n.timeEnd,
-              _session.timeEnd != null
-                  ? MaterialLocalizations.of(context).formatTimeOfDay(_session.timeEnd!)
-                  : '-/-',
-            ),
-            _buildInfoRow(
-              context,
-              i18n.impression,
-              _session.impressionAsString,
-            ),
-            _buildInfoRow(
-              context,
-              i18n.notes,
-              _session.notes.isNotEmpty ? _session.notes : '-/-',
-            ),
-          ],
-        ),
+        ],
       ),
     );
   }
@@ -99,12 +113,7 @@ class SessionInfo extends StatelessWidget {
             '$label: ',
             style: Theme.of(context).textTheme.titleMedium?.copyWith(fontWeight: FontWeight.bold),
           ),
-          Expanded(
-            child: Text(
-              value,
-              style: Theme.of(context).textTheme.titleMedium,
-            ),
-          ),
+          Expanded(child: Text(value)),
         ],
       ),
     );
@@ -206,115 +215,4 @@ class DayLogWidget extends StatelessWidget {
       ),
     );
   }
-}
-void showEditSessionDialog(BuildContext context, WorkoutSession session, RoutinesProvider routinesProvider) {
-  final _formKey = GlobalKey<FormState>();
-  final notesController = TextEditingController(text: session.notes);
-  final timeStartController = TextEditingController(text: timeToString(session.timeStart ?? TimeOfDay.now())!);
-  final timeEndController = TextEditingController(text: timeToString(session.timeEnd ?? TimeOfDay.now())!);
-  List<bool> selectedImpression = [false, false, false];
-  selectedImpression[session.impression - 1] = true;
-
-  showDialog(
-    context: context,
-    builder: (ctx) {
-      return AlertDialog(
-        title: Text('Edit Session (${session.date.toLocal().toString().split(' ')[0]})'),
-        content: Form(
-          key: _formKey,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ToggleButtons(
-                  renderBorder: false,
-                  isSelected: selectedImpression,
-                  onPressed: (index) {
-                    for (int i = 0; i < selectedImpression.length; i++) {
-                      selectedImpression[i] = (i == index);
-                    }
-                    session.impression = index + 1;
-                  },
-                  children: const [
-                    Icon(Icons.sentiment_very_dissatisfied),
-                    Icon(Icons.sentiment_neutral),
-                    Icon(Icons.sentiment_very_satisfied),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Notes'),
-                  controller: notesController,
-                  maxLines: 3,
-                  onSaved: (value) => session.notes = value ?? '',
-                ),
-                const SizedBox(height: 10),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'Start Time'),
-                  controller: timeStartController,
-                  onTap: () async {
-                    FocusScope.of(ctx).requestFocus(FocusNode());
-                    final picked = await showTimePicker(
-                      context: ctx,
-                      initialTime: session.timeStart ?? TimeOfDay.now(),
-                    );
-                    if (picked != null) {
-                      timeStartController.text = timeToString(picked)!;
-                      session.timeStart = picked;
-                    }
-                  },
-                  validator: (_) {
-                    final start = stringToTime(timeStartController.text);
-                    final end = stringToTime(timeEndController.text);
-                    if (start.isAfter(end)) {
-                      return 'Start time cannot be after end time.';
-                    }
-                    return null;
-                  },
-                  onSaved: (val) => session.timeStart = stringToTime(val),
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(labelText: 'End Time'),
-                  controller: timeEndController,
-                  onTap: () async {
-                    FocusScope.of(ctx).requestFocus(FocusNode());
-                    final picked = await showTimePicker(
-                      context: ctx,
-                      initialTime: session.timeEnd ?? TimeOfDay.now(),
-                    );
-                    if (picked != null) {
-                      timeEndController.text = timeToString(picked)!;
-                      session.timeEnd = picked;
-                    }
-                  },
-                  onSaved: (val) => session.timeEnd = stringToTime(val),
-                ),
-              ],
-            ),
-          ),
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.of(ctx).pop(),
-            child: const Text('Cancel'),
-          ),
-          ElevatedButton(
-            onPressed: () async {
-              if (_formKey.currentState?.validate() ?? false) {
-                _formKey.currentState?.save();
-
-                try {
-                  await routinesProvider.editSession(session);
-                  Navigator.of(ctx).pop();
-                } catch (e) {
-                  showErrorDialog(e, context);
-                }
-              }
-            },
-            child: const Text('Save'),
-          ),
-        ],
-      );
-    },
-  );
 }
