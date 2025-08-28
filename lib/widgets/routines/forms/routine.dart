76adb1aa8ff365dceb7157952893e460bf569ed4
Roland Geider <roland@geider.net>
2025-08-28 22:47:31 +0200

    Localize field names in routine form

--- lib/widgets/routines/forms/routine.dart
+++ lib/widgets/routines/forms/routine.dart
@@ -108,8 +108,8 @@ class _RoutineFormState extends State<RoutineForm> {
           }
           return null;
         },
-        decoration: const InputDecoration(
-          labelText: 'Start date',
+        decoration: InputDecoration(
+          labelText: i18n.startDate,
           suffixIcon: Icon(
             Icons.calendar_today,
             key: Key('calendarIcon'),
@@ -145,8 +145,8 @@ class _RoutineFormState extends State<RoutineForm> {
         key: const Key('field-end-date'),
         // Stop keyboard from appearing
         readOnly: true,
-        decoration: const InputDecoration(
-          labelText: 'End date',
+        decoration: InputDecoration(
+          labelText: i18n.endDate,
           suffixIcon: Icon(
             Icons.calendar_today,
             key: Key('calendarIcon'),
