cbdc4a0c561b438f9ef3d1ec43048d1944a69baa
Roland Geider <roland@geider.net>
2026-01-12 21:56:59 +0100

    Increase page size for languages, no need to use pagination for this
    Categories and muscles will probably never get so big, but it doesn't do any
    harm doing it as well.

--- test/exercises/exercise_provider_db_test.dart
+++ test/exercises/exercise_provider_db_test.dart
@@ -1,3 +1,21 @@
+/*
+ * This file is part of wger Workout Manager <https://github.com/wger-project>.
+ * Copyright (c)  2026 wger Team
+ *
+ * wger Workout Manager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import 'dart:convert';
 
 import 'package:drift/native.dart';
@@ -100,19 +118,25 @@ void main() {
     SharedPreferencesAsyncPlatform.instance = InMemorySharedPreferencesAsync.empty();
 
     // Mock categories
-    when(mockBaseProvider.makeUrl(categoryUrl)).thenReturn(tCategoryEntriesUri);
+    when(
+      mockBaseProvider.makeUrl(categoryUrl, query: anyNamed('query')),
+    ).thenReturn(tCategoryEntriesUri);
     when(
       mockBaseProvider.fetchPaginated(tCategoryEntriesUri),
     ).thenAnswer((_) => Future.value(tCategoryMap['results']));
 
     // Mock muscles
-    when(mockBaseProvider.makeUrl(muscleUrl)).thenReturn(tMuscleEntriesUri);
+    when(
+      mockBaseProvider.makeUrl(muscleUrl, query: anyNamed('query')),
+    ).thenReturn(tMuscleEntriesUri);
     when(
       mockBaseProvider.fetchPaginated(tMuscleEntriesUri),
     ).thenAnswer((_) => Future.value(tMuscleMap['results']));
 
     // Mock equipment
-    when(mockBaseProvider.makeUrl(equipmentUrl)).thenReturn(tEquipmentEntriesUri);
+    when(
+      mockBaseProvider.makeUrl(equipmentUrl, query: anyNamed('query')),
+    ).thenReturn(tEquipmentEntriesUri);
     when(
       mockBaseProvider.fetchPaginated(tEquipmentEntriesUri),
     ).thenAnswer((_) => Future.value(tEquipmentMap['results']));
