6d9ead5c9b520331d58d768714f53676cda5914c
Roland Geider <roland@geider.net>
2026-06-15 22:55:20 +0200

    Mark models as immutable

--- lib/models/workouts/repetition_unit.dart
+++ lib/models/workouts/repetition_unit.dart
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import 'package:flutter/foundation.dart';
+
+@immutable
 class RepetitionUnit {
   final int id;
   final String name;
--- lib/models/workouts/weight_unit.dart
+++ lib/models/workouts/weight_unit.dart
@@ -16,6 +16,9 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+import 'package:flutter/foundation.dart';
+
+@immutable
 class WeightUnit {
   final int id;
   final String name;
